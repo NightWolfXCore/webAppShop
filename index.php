922cc1579167b14c354a69e385a94f95b92d0b99
Strel04nik <alexzavalin15911@gmail.com>
2026-05-23 15:37:26 +0700

    1

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once "src/repository/database.php";
-require_once "src/model/dataUser.php";
-require_once "src/model/authentication.php";
-require_once "src/router/routing.php";
-require_once "src/router/router.php";
-?>  
